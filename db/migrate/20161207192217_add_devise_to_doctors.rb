4676876cb668146b3e7eb6ad35149240cd46bc15
h13knsd <harish.r1729@gmail.com>
2016-12-16 14:35:42 -0500

    commit

--- db/migrate/20161207192217_add_devise_to_doctors.rb
+++ db/migrate/20161207192217_add_devise_to_doctors.rb
@@ -2,7 +2,6 @@ class AddDeviseToDoctors < ActiveRecord::Migration[5.0]
   def self.up
     change_table :doctors do |t|
       ## Database authenticatable
-      t.string :name,               null: false, default: ""
       t.string :email,              null: false, default: ""
       t.string :encrypted_password, null: false, default: ""
 
